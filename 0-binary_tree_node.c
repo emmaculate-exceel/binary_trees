532f219b556793d786b80013a15a865448466a34
emmaculate-exceel <emmaculate2exceel@live.com>
2024-02-28 10:06:13 +0100

    added info

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_node - struct that creates a node
+ *@parent: pointer to the parent node of the node to create
+ *@value: value to be passed into the new node
+ *
+ *Return: always 0;
+ */
+
+binary_tree_t binary_tree_t_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = (binary_tree_t)malloc(sizeof(binary_tree_t binary_tree_t_node));
+
+	if (newnode == NULL)
+	   return NULL;
+
+	newnode->right = NULL;
+	newnode->left = NULL;
+	newnode->parent = parent;
+	newnode->n = value;
+
+	return (*newnode);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,12 +1,12 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-##standard library
+//standard library
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
-##Basic Binary Tree
+//Basic Binary Tree
 /**
  * struct binary_trees_s - Binary tree node
  *
@@ -23,22 +23,23 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 	struct binary_tree_s *left;
 };
-##Typedefine for Binary Tree
+//Typedefine for Binary Tree
 typedef struct binary_tree_s binary_tree_t;
 
-##Typdefine for Binary Search Tree
+//Typdefine for Binary Search Tree
 typedef struct binary_tree_s bst_t;
 
-##Typedefine for Avl Tree
+//Typedefine for Avl Tree
 typedef struct binary_tree_s avl_t;
 
-##Typedefine for Binary Heap
+//Typedefine for Binary Heap
 typedef struct binary_tree_s heap_t;
+/// binary_print_functions for the code
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_t *binary_tree_delete(binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int));
@@ -55,5 +56,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-###advance task for the project ;
+//advance task for the project ;
 #endif 
